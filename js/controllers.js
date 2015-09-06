297432182826ceb1ca082ec08ad37096e4af33fa
碧城 <95700950@qq.com>
2015-09-06 17:10:45 +0800

    good detail make

--- js/controllers.js
+++ js/controllers.js
@@ -187,7 +187,10 @@ angular.module('starter.controllers', [])
   })
 })
 
-.controller('GoodDetailCtrl', function($scope, $stateParams, Chats) {
+.controller('GoodDetailCtrl', function($scope, $stateParams, $ionicLoading) {
+  $ionicLoading.show({
+      template: '加载中...'
+  });
   var Cgood={};
   var goodDetail= new AV.Query('Goods');
   goodDetail.equalTo('objectId',$stateParams.goodId)
@@ -196,16 +199,18 @@ angular.module('starter.controllers', [])
     var shop=new AV.Query('Shops');
     shop.equalTo('objectId',good.toJSON().shopID)
     shop.first().then(function(shop){
-      Cgood.shopinfo=shop.toJSON()
+      Cgood.shopinfo=shop.toJSON();
+        $scope.$apply(function(){
+        $scope.good=Cgood;
+      })
     }).catch(function(err){
     alert('商家出错了~')
     })
     console.log(Cgood);
-    $scope.$apply(function(){
-      $scope.good=Cgood;
-    })
   }).catch(function(err){
     alert('产品详情出错了~')
+  }).finally(function(){
+      $ionicLoading.hide();//loading结束
   });
 
 
